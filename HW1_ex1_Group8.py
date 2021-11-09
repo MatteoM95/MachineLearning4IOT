ef6d5118c5a36eeaaaf3a21eccc67d183e5eb179
MatteoM95 <1906hk@gmail.com>
2021-11-09 17:04:15 +0100

    normalize args added

--- HW1_ex1_Group8.py
+++ HW1_ex1_Group8.py
@@ -2,12 +2,11 @@ import os
 import argparse
 import tensorflow as tf
 from datetime import datetime
-import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", type=str, default="./dataset", help="Input File Name. Default: ./raw_data")
 parser.add_argument("--output", type=str, default="./th.tfrecord", help="Output File Name. Default: ./th.tfreord")
-parser.add_argument("--normalize", type=str, default="False", help="Normalize Temperature: --normalize == True")
+parser.add_argument("--normalize", type=str, default="False", help="Normalize Temperature? Insert --normalize")
 args = parser.parse_args()
 
 output_filename = args.output
@@ -17,10 +16,10 @@ normalize = args.normalize
 tMAX = 0
 tMIN = 9999999
 
-def normalizeFunc(tmp):
+def normalize_func(tmp):
     if tMAX > tmp:
         tMAX = tmp
-    if tMIN < tmp
+    if tMIN < tmp:
         tMIN = tmp
     norm = (tmp - tMIN) / (tMAX - tMIN)
     return norm
@@ -31,28 +30,29 @@ def main():
             lines = fp.readlines()
             for line in lines:
                 if not line.isspace():
-                    date, time, temperature, humidity, audioName = line.strip().split(",")
+                    date, time, temperature, humidity = line.strip().split(",")
                     day, month, year = date.split("/")
                     hour, minutes, seconds = time.split(":")
 
                     timestamp = datetime(int(year), int(month), int(day), int(hour), int(minutes), int(seconds)).timestamp()
-                    audio_path = os.path.join(input_dir, audioName)
-                    audio = tf.io.read_file(audio_path)
 
+                    #Normalization of temperature if required
                     if normalize:
-                        temperatureNorm = normalizeFunc(temperature)
+                        temperatureNorm = normalize_func(temperature)
 
+                    #Conversion to best format for saving HDD space
+                    #TODO
 
-                    timestamp_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(timestamp)]))
-                    temperature_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(temperature)]))
-                    humidity_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(humidity)]))
-                    audio_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[audio.numpy()]))
-                    mapping = {'timestamp': timestamp_feature, \
-                               'temperature': temperature_feature, \
-                               'humidity': humidity_feature, \
-                               'audio': audio_feature}
-                    payload = tf.train.Example(features=tf.train.Features(feature=mapping))
-                    writer.write(payload.SerializeToString())
+                    # timestamp_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(timestamp)]))
+                    # temperature_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(temperature)]))
+                    # humidity_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(humidity)]))
+                    #
+                    # mapping = {'timestamp': timestamp_feature, \
+                    #            'temperature': temperature_feature, \
+                    #            'humidity': humidity_feature
+                    #            }
+                    # payload = tf.train.Example(features=tf.train.Features(feature=mapping))
+                    # writer.write(payload.SerializeToString())
 
     print(os.path.getsize(output_filename))
 
