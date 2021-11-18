3b6f204f28f09aff85962007094644aded119f54
MatteoM95 <1906hk@gmail.com>
2021-11-18 20:14:10 +0100

    fix shape problem

--- HW1/HW1_ex2_Group8.py
+++ HW1/HW1_ex2_Group8.py
@@ -20,18 +20,25 @@ def audio_processing(filename, stftParams, mfccParams, num_coefficients, resampl
 
         tf_audio = tf.convert_to_tensor(audio, dtype=tf.float32)
 
+        # stftParams = {'frame_length': 1,
+        #                'frame_step': 2}
+
+        stft = tf.signal.stft(tf_audio,
+                              frame_length=stftParams['frame_length'],
+                              frame_step=stftParams['frame_step'],
+                              fft_length=frame_length)
+        spectrogram = tf.abs(stft)
+
     else:
         audio = tf.io.read_file(filename)
         tf_audio, _ = tf.audio.decode_wav(audio)
         tf_audio = tf.squeeze(tf_audio, 1)
 
-    # print("audio: ", tf_audio.shape)
-    stft = tf.signal.stft(tf_audio,
-                          frame_length=stftParams['frame_length'],
-                          frame_step=stftParams['frame_step'],
-                          fft_length=frame_length)
-    spectrogram = tf.abs(stft)
-    # print("spectogram: ", spectrogram.shape)
+        stft = tf.signal.stft(tf_audio,
+                              frame_length=stftParams['frame_length'],
+                              frame_step=stftParams['frame_step'],
+                              fft_length=frame_length)
+        spectrogram = tf.abs(stft)
 
     num_spectrogram_bins = spectrogram.shape[-1]
     linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(num_mel_bins=mfccParams['num_mel_bins'],
@@ -39,12 +46,10 @@ def audio_processing(filename, stftParams, mfccParams, num_coefficients, resampl
                                                                         sample_rate=mfccParams['sampling_rate'],
                                                                         lower_edge_hertz=mfccParams['lower_frequency'],
                                                                         upper_edge_hertz=mfccParams['upper_frequency'])
-    # print("linear_to_mel_weight_matrix: ", linear_to_mel_weight_matrix.shape)
 
     mel_spectrogram = tf.tensordot(spectrogram,
                                    linear_to_mel_weight_matrix,
                                    1)
-    # print("mel_spectrogram: ", mel_spectrogram.shape)
 
     mel_spectrogram.set_shape(spectrogram.shape[:-1].concatenate(linear_to_mel_weight_matrix.shape[-1:]))
 
@@ -60,11 +65,15 @@ Popen('sudo sh -c "echo performance >'
       shell=True).wait()
 
 path = "../datasets/yes_no"
-#path = "/content/yesNo_"
+# path = "/content/yesNo_"
 
-# STFT parameters
-sftf_param = {'frame_length': 8,
-              'frame_step': 16}
+# STFT parameters slow
+sftf_param_slow = {'frame_length': 8,
+                   'frame_step': 16}
+
+# STFT parameters fast
+sftf_param_fast = {'frame_length': 8,
+                   'frame_step': 16}
 
 # MFCC_slow parameters
 mfccSlow_param = {'num_mel_bins': 40,
@@ -73,10 +82,10 @@ mfccSlow_param = {'num_mel_bins': 40,
                   'sampling_rate': 16000}
 
 # MFCC_fast parameters
-mfccFast_param = {'num_mel_bins': 40,
-                  'lower_frequency': 20,
-                  'upper_frequency': 4000,
-                  'sampling_rate': 8000}
+mfccFast_param = {'num_mel_bins': 30,
+                  'lower_frequency': 200,
+                  'upper_frequency': 1500,
+                  'sampling_rate': 3000}
 
 num_coefficients = 10
 
@@ -87,12 +96,12 @@ num_file = 0
 
 for file in os.listdir(path):
     start = time.time()
-    mfccSlow = audio_processing(os.path.join(path, file), sftf_param, mfccSlow_param, num_coefficients)
+    mfccSlow = audio_processing(os.path.join(path, file), sftf_param_slow, mfccSlow_param, num_coefficients)
     end = time.time()
     mfccSlow_execTime += end - start
 
     start = time.time()
-    mfccFast = audio_processing(os.path.join(path, file), sftf_param, mfccFast_param, num_coefficients) #, True, 8000)
+    mfccFast = audio_processing(os.path.join(path, file), sftf_param_fast, mfccFast_param, num_coefficients) #, True, 8000)
     end = time.time()
     mfccFast_execTime += end - start
 
@@ -100,17 +109,16 @@ for file in os.listdir(path):
 
     num_file += 1
 
-print(mfccSlow.shape, mfccFast.shape)
-mfccFast = tf.broadcast_to(mfccFast, mfccSlow.shape)
-assert mfccSlow.shape == mfccFast.shape, "The shape of MFCCslow != shape of MFCCfast"
-# assert tf.shape(mfccSlow) == tf.shape(mfccFast), "The shape of MFCCslow != shape of MFCCfast"
+print("MFCC shape: ", mfccSlow.shape, mfccFast.shape)
 
-#SNR = 20 * math.log10(np.linalg.norm(mfccSlow) / np.linalg.norm(tf.subtract(mfccSlow - mfccFast) + 1e-6))
+# SNR = 20 * math.log10(np.linalg.norm(mfccSlow) / np.linalg.norm(tf.subtract(mfccSlow - mfccFast) + 1e-6))
 
 print(f"MFCC slow = {mfccSlow_execTime / num_file * 1000} ms")
 print(f"MFCC fast = {mfccFast_execTime / num_file * 1000} ms")
 print(f"SNR = {SNR / num_file} dB")
 
+assert mfccSlow.shape == mfccFast.shape, "The shape of MFCCslow != shape of MFCCfast"
 assert SNR / num_file > 10.40, "SNR is < 10.40!"
-
 assert mfccFast_execTime / num_file * 1000 > 18, "Attention, MFCCfast is not so fast"
+
+# assert tf.shape(mfccSlow) == tf.shape(mfccFast), "The shape of MFCCslow != shape of MFCCfast"
