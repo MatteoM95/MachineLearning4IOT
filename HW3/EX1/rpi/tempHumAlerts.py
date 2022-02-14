a9378dc5b1bf7991cc4b1f7c4f51d70accb9f032
Gabrysse <34710621+Gabrysse@users.noreply.github.com>
2022-02-14 16:20:27 +0100

    Improvments to HW3/EX1

--- HW3/EX1/rpi/tempHumAlerts.py
+++ HW3/EX1/rpi/tempHumAlerts.py
@@ -40,54 +40,61 @@ def begin(model, tthresh, hthresh):
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
+    MEAN = np.array([9.107597, 75.904076], dtype=np.float32)
+    STD = np.array([8.654227, 16.557089], dtype=np.float32)
 
     dht_device = adafruit_dht.DHT11(D4)
     while True:
-        input = np.zeros([1, 6, 2], dtype=np.float32)
+        window = np.zeros([1, 6, 2], dtype=np.float32)
         i = 0
-        while i < 6:
+        if i < 6:
             # Try except in order to manage occasional sensor failure
             try:
-                input[0, i, 0] = dht_device.temperature
-                input[0, i, 1] = dht_device.humidity
+                window[0, i, 0] = dht_device.temperature
+                window[0, i, 1] = dht_device.humidity
                 time.sleep(1)
                 i += 1
             except:
                 pass
+        else:
+            # Try except in order to manage occasional sensor failure
+            try:
+                y_true = np.array([dht_device.temperature, dht_device.humidity])
+            except:
+                time.sleep(2)
+                y_true = np.array([dht_device.temperature, dht_device.humidity])
 
-        # Try except in order to manage occasional sensor failure
-        try:
-            y_true = np.array([dht_device.temperature, dht_device.humidity])
-        except:
-            time.sleep(2)
-            y_true = np.array([dht_device.temperature, dht_device.humidity])
+            window = (window - MEAN) / STD
+            interpreter.set_tensor(input_details[0]['index'], window)
+            interpreter.invoke()
+            prediction = interpreter.get_tensor(output_details[0]['index']).reshape(2, )
 
-        interpreter.set_tensor(input_details[0]['index'], input)
-        interpreter.invoke()
-        prediction = interpreter.get_tensor(output_details[0]['index']).reshape(2,)
+            abs_error = np.abs(prediction - y_true)
+            print(abs_error)
 
-        abs_error = np.abs(prediction - y_true)
-        print(abs_error)
+            if abs_error[0] > tthresh:
+                response = {
+                    "bn": "Temperature Alert",
+                    "bt": int(datetime.now().timestamp()),
+                    "e": [
+                        {"n": "pred", "u": "°C", "t": 0, "v": str(prediction[0])},
+                        {"n": "actual", "u": "°C", "t": 0, "v": str(y_true[0])}
+                    ]
+                }
+                alerts.myPublish("/alerts", json.dumps(response))
+            if abs_error[1] > hthresh:
+                response = {
+                    "bn": "Humidity Alert",
+                    "bt": int(datetime.now().timestamp()),
+                    "e": [
+                        {"n": "pred", "u": "%", "t": 0, "v": str(prediction[1])},
+                        {"n": "actual", "u": "%", "t": 0, "v": str(y_true[1])}
+                    ]
+                }
+                alerts.myPublish("/alerts", json.dumps(response))
 
-        if abs_error[0] > tthresh:
-            response = {
-                "bn": "Temperature Alert",
-                "bt": int(datetime.now().timestamp()),
-                "e": [
-                    {"n": "pred", "u": "°C", "t": 0, "v": str(prediction[0])},
-                    {"n": "actual", "u": "°C", "t": 0, "v": str(y_true[0])}
-                ]
-            }
-            alerts.myPublish("/alerts", json.dumps(response))
-        if abs_error[1] > hthresh:
-            response = {
-                "bn": "Humidity Alert",
-                "bt": int(datetime.now().timestamp()),
-                "e": [
-                    {"n": "pred", "u": "%", "t": 0, "v": str(prediction[1])},
-                    {"n": "actual", "u": "%", "t": 0, "v": str(y_true[1])}
-                ]
-            }
-            alerts.myPublish("/alerts", json.dumps(response))
+            window[:, 0:5, :] = window[:, 1:6, :]
+            window[:, -1, 0] = y_true[0]
+            window[:, -1, 1] = y_true[1]
 
     alerts.stop()
